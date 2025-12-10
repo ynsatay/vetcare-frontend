f391bde8eff520ea8c34f69b4d6036e0f8a205bc
MehmetCan <mehmetcan3742@gmail.com>
2025-12-10 13:30:23 +0300

    düzeltme

--- src/config.js
+++ src/config.js
@@ -1,10 +1,19 @@
 const hostname = window.location.hostname;
 
+// Log ekledik
+console.log("Current Hostname:", hostname);
+
 const isLocal =
   hostname === "localhost" ||
   hostname === "127.0.0.1" ||
   hostname === "";
 
+// Local kontrol logu
+console.log("isLocal:", isLocal);
+
 export const BASE_URL = isLocal
   ? "http://localhost:3001/api"
   : "https://vetcaretr.com/api";
+
+// Sonuç olarak hangi URL kullanılıyor onu da logla
+console.log("BASE_URL:", BASE_URL);
