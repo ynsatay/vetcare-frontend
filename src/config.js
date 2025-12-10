e6afc5399f6f02c87688226843d84ddac94e0bb9
MehmetCan <mehmetcan3742@gmail.com>
2025-12-10 13:26:45 +0300

    düzenleme

--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
-const isLocal = window.location.hostname === "localhost";
+const hostname = window.location.hostname;
+
+const isLocal =
+  hostname === "localhost" ||
+  hostname === "127.0.0.1" ||
+  hostname === "";
 
 export const BASE_URL = isLocal
   ? "http://localhost:3001/api"
