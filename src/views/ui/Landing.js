dd0b21bcbf4c3345f54eebbbd50d934c4e223eab
MehmetCan <mehmetcan3742@gmail.com>
2025-12-15 00:32:17 +0300

    video

--- src/views/ui/Landing.js
+++ src/views/ui/Landing.js
@@ -1235,7 +1235,7 @@ const Landing = () => {
                                 <iframe
                                     width="100%"
                                     height="100%"
-                                    src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+                                    src="https://www.youtube.com/embed/jxXw2GRlN2M"
                                     title={t('VideoTitle')}
                                     frameBorder="0"
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
